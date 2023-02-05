534024a33f059ca2e07884ecaa5ef277d69fa405
Merlin033 <barnabedebocq@live.fr>
2023-02-05 01:41:55 +0100

    finished but miss one thing --> README

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const submitBtn = document.querySelector(".btn-submit");
+const ratingInputs = document.querySelectorAll('input[name="rating"]');
+const ratingContainer = document.querySelector(".rating-container");
+const thanksContainer = document.querySelector(".thanks-container");
+const result = document.querySelector(".rate");
+
+submitBtn.addEventListener("click", function () {
+  let selectedRating;
+  for (let i = 0; i < ratingInputs.length; i++) {
+    if (ratingInputs[i].checked) {
+      selectedRating = ratingInputs[i].value;
+      break;
+    }
+  }
+
+  if (selectedRating) {
+    ratingContainer.style.display = "none";
+    thanksContainer.style.display = "grid";
+    result.insertAdjacentHTML(
+      "beforeend",
+      "&nbsp;" + selectedRating + "&nbsp;"
+    );
+  }
+});
+
 // <!-- Thank you state start -->
 
 //     You selected
